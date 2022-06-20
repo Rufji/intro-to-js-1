deeecd1527d7dcd6bfee0c239889aba897c0c11e
Rufji <mahmudrufai05@gmail.com>
2022-06-20 16:40:13 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var name = 'Rufai mahmud';
+console.log("my name is "+name)
+alert("my name is "+name)
+var height = "5'8";
+console.log("i am "+height)
+alert("i am "+height)
+var country = "Nigeria"
+console.log("the name of my country is "+country)
+alert("the name of my country is "+country)
